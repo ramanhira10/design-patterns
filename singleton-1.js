84841e0f9a569c3a3c2cda89aa84887d81799c5e
Raman Hira <ramanhira@gmail.com>
2022-07-16 10:35:43 +0530

    demonstrated singleton pattern using object literal

--- singleton-1.js
+++ singleton-1.js
@@ -1,1 +1,14 @@
-console.log('going to demonstrate singleton');
+/** Singleton Pattern using object literal */
+
+const Config = {
+    start: () => console.log('App has started'),
+    update: () => console.log('App has updated')
+};
+
+Object.freeze(Config);
+
+Config.start();
+Config.update();
+
+Config.name = 'Test Name';
+console.log(Config);
