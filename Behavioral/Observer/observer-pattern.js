0969f02512e100e696088f6ffca63ef9b9618c39
Raman Hira <ramanhira@gmail.com>
2023-05-18 14:12:18 +0530

    updated for observer pattern

---
+++ Behavioral/Observer/observer-pattern.js
@@ -0,0 +1,5 @@
+/**
+ * The observer pattern lets you define a subscription mechanism to notify multiple objects about any events that happen to the object they are observing. Basically, it's like having an event listener on a given object, and when that object perfoms the action we're listening for, we do something.
+ * 
+ * Simple example is useEffect hook in React
+ */
