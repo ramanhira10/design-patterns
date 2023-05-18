743271aa3f018824916285dc3c490e7791cbf1c4
Raman Hira <ramanhira@gmail.com>
2023-05-18 13:24:15 +0530

    updated for abstract-factory

--- Creational/Abstract/abstract-factory.js
+++ Creational/Abstract/abstract-factory.js
@@ -1,4 +1,10 @@
-/** Abstract Factory Method pattern */
+/** Abstract Factory Method pattern
+ * The pattern allow us to produce families of related objects without specifying concrete classes. It's useful in situations where we need to create objects that share only some properties and methods.
+ * 
+ * The way it works is by presenting an abstract factory the client interacts with. The abstract factory calls the corresponding concrete factory given the corresponding logic. And the concrete factory is one that returns the end object.
+ * 
+ * Basically it just adds an abstraction layer over the factory method pattern, so that we can create many different types of objecs, but still interact with a single factory function or class.
+ */
 
 class Car {
 
